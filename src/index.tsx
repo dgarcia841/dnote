ababa53f757f2f0ac34ea51363406f5cb78e624f
dgarcia841 <dgarcia841@unab.edu.co>
2022-05-20 23:59:42 +0000

    load storage at app load

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Client from "react-dom/client"
 import App from "@src/components/App"
+import { Storage } from "./storage";
 
 window.addEventListener("load", () => {
+    Storage.load();
     const root = document.getElementById("app-root");
     if (!root) {
         document.write("App root not found");
